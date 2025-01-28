6cc52beb1cfbcbd1516a83024c72baa884399a3f
hallousse.yehya <yehya.hallousse@itexc-agency.com>
2025-01-28 17:04:41 +0100

    controler

--- src/testings/testings.controller.ts
+++ src/testings/testings.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
 @Controller('testings')
-export class TestingsController {}
+export class TestingsController {
+  // Using findAll: indicates it may include filtering logic.
+  @Get()
+  findAll(@Query() query: any) {
+    return [
+      'This will return all testings data (potentially with filters).',
+      query,
+    ];
+  }
+
+  // Using getAll: suggests no filtering logic.
+  @Get('all')
+  getAll() {
+    return 'This will return all testings data exactly as stored.';
+  }
+
+  // Using findOne: indicates searching for ajust one testing data with custom criteria.
+  @Get('find/:name')
+  findOne(@Param('name') name: string) {
+    return `This will search and return one testing data with name: ${name}`;
+  }
+
+  // Using getOne: indicates direct retrieval by ID.
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return `This will return one testing data with ID: ${id}`;
+  }
+}
