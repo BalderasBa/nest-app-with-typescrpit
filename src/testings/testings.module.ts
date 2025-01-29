6240c5107dd91df81ba4aefa9b9dc2c31eb25ce9
hallousse.yehya <yehya.hallousse@itexc-agency.com>
2025-01-29 11:38:46 +0100

    3. Provider/service

--- src/testings/testings.module.ts
+++ src/testings/testings.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TestingsController } from './testings.controller';
 import { TestingsService } from './testings.service';
-import { SubTestModule } from './sub-test/sub-test.module';
 
 @Module({
   controllers: [TestingsController],
   providers: [TestingsService],
-  imports: [SubTestModule],
+  imports: [],
 })
 export class TestingsModule {}
